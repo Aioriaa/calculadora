df87354c1e4ab0560d4d8c4ad9568d052d7ed86e
Eduardo Garcia <galeanoeduardo62@gmail.com>
2021-08-01 22:08:05 -0500

    trabajo de calculadora

---
+++ trabajo.py
@@ -0,0 +1,28 @@
+def operaciones(numeros):
+     numero = int(input("Escribe un numero: "))
+     numero2 = int(input("Escribe un numero: "))
+     operacion = 
+     print("el resultado es " + str(operacion)) 
+
+
+
+menu = """
+Bienvenidos a la calculadora
+1 - suma
+2 - resta
+3 - multiplicacion 
+4 - divicion 
+Elige una opcion porfavor: """
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    operaciones()
+elif opcion == 2:
+   operaciones() 
+elif opcion == 3:
+    operaciones()
+elif opcion == 4:
+    operaciones()
+else:
+    print("Eliga una opcion correcta por favor")
